26e9cb70d013c0ff3737c63b32bf99d0fae7ddea
henok <heni.hadush@gmail.com>
2025-07-13 20:25:10 +0300

    updated one 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,9 +3,9 @@ const args = process.argv.slice(2);
 if (args[0] === undefined) {
   console.log('No argument');
 } else if (args[1] === undefined) {
-  console.log('HBNT');
-} else if (args[2] === 'HBNT' && args[1] === 'cool') {
-  console.log('HBNT' + ' ' + 'cool');
+  console.log('HBTN');
+} else if (args[2] === 'HBTN' && args[1] === 'cool') {
+  console.log('HBTN' + ' ' + 'cool');
 }
 
 

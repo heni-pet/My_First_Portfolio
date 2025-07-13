b1ff65c73cf056ddea7c54ebc07733525dc87d53
henok <heni.hadush@gmail.com>
2025-07-13 15:01:31 +0300

    update added 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,11 +4,6 @@ if (args[0] === undefined) {
   console.log('No argument');
 } else if (args[1] === undefined) {
   console.log('Argument found');
-  console.log('Value:', args[0]);
 } else {
   console.log('Arguments found');
-  console.log('Values:');
-  console.log(args[0]);
-  console.log(args[1]);
-  if (args[2] !== undefined) console.log('...and more');
 }

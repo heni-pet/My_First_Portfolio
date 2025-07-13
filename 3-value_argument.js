929890c979c51be34a11b422c129a4c1dc986544
henok <heni.hadush@gmail.com>
2025-07-13 20:17:39 +0300

    updated one 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,7 +4,7 @@ if (args[0] === undefined) {
   console.log('No argument');
 } else if (args[1] === undefined) {
   console.log('HBNT');
-} else {
+} else if (args[2] === 'HBNT' && args[1] === 'cool') {
   console.log('HBNT' + ' ' + 'cool');
 }
 
